06503ff5d9dd048a053a6e36dbbc61803a363448
rafaelngoncalves5 <rafaelngoncalves5@outlook.com>
2023-11-04 22:21:14 -0300

    Validations

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,19 +4,39 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\View\View;
+use Illuminate\Validation\Rules\Password;
 
 class UserController extends Controller
 {
-    public function create(Request $request, User $user)
+    public function create(Request $request, User $user): view
     {
         if ($request->method() === 'GET') {
             return view('users.create');
 
         } else if ($request->method() === 'POST') {
+
+            $request->validate([
+                'username' => 'required|unique:users|filled|string',
+                'email' => ['required', 'email', 'unique:users', 'filled', 'confirmed'],
+                'password' => [
+                    'required',
+                    'filled',
+                    'string',
+                    'confirmed',
+                    Password::min(8)
+                        ->mixedCase()
+                        ->numbers()
+                        ->symbols()
+                        ->uncompromised()
+                ]
+            ]);
+
             $new_user = User::create($request->all());
-            return view('users.create', ['new_user' => $new_user]);
+            return view('success', ['feedback' => "User $new_user->username created with success!"]);
+
         } else {
-            return view(abort(405, "Erros: method not allowed!"));
+            return view(abort(405, "Error: method not allowed!"));
         }
     }
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -12,13 +12,22 @@
             @csrf
 
             <label for='username'>Username:</label>
-            <input id='username' name='username' placeholder='johndoe5'></input>
+            <input id='username' name='username' placeholder='johndoe5' value={{ old('username') }}></input>
 
             <label for='email'>Email:</label>
-            <input id='email' name='email' placeholder='johndoe5@mail.com'></input>
+            <input id='email' name='email' placeholder='johndoe5@mail.com' value={{ old('email') }}></input>
+            <label for='email_confirmation'>Email confirmation:</label>
+            <input id='email_confirmation' name='email_confirmation' placeholder='johndoe5@mail.com'></input>
+
 
             <label for='password'>Password:</label>
             <input id='password' name='password' type='password'></input>
+            <label for='password_confirmation'>Password confirmation:</label>
+            <input id='password_confirmation' name='password_confirmation' type='password'></input>
+
+            @foreach($errors->all() as $error)
+            <span class='error'><strong> - </strong>{{ $error }}</span>
+            @endforeach
 
             <button type='submit'>Submit</button>
         </form>
